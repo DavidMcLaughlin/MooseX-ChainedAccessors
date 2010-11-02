1fdad1c81702ec1bb0148739e5f501da39fb1064
Shlomi Fish <shlomif@iglu.org.il>
2010-11-03 01:25:25 +0800

    Applied the fix from bug #55087.
    See:
    
    https://rt.cpan.org/Ticket/Display.html?id=55087
    
    This fixes the tests with recent Mooses.

--- lib/MooseX/ChainedAccessors/Accessor.pm
+++ lib/MooseX/ChainedAccessors/Accessor.pm
@@ -6,51 +6,8 @@ use base 'Moose::Meta::Method::Accessor';
 
 our $VERSION = '0.01';
 
-sub _generate_accessor_method_inline {
-    my $self        = $_[0];
-    my $attr        = $self->associated_attribute;
-    my $attr_name   = $attr->name;
-    my $inv         = '$_[0]';
-    my $slot_access = $self->_inline_access($inv, $attr_name);
-    my $value_name  = $self->_value_needs_copy ? '$val' : '$_[1]';
-
-    $self->_eval_code('sub { ' . "\n"
-    . $self->_inline_pre_body(@_) . "\n"
-    . 'if (scalar(@_) >= 2) {' . "\n"
-        . $self->_inline_copy_value . "\n"
-        . $self->_inline_check_required . "\n"
-        . $self->_inline_check_coercion($value_name) . "\n"
-        . $self->_inline_check_constraint($value_name) . "\n"
-        . $self->_inline_get_old_value_for_trigger($inv, $value_name) . "\n"
-        . $self->_inline_store($inv, $value_name) . "\n"
-        . $self->_inline_trigger($inv, $value_name, '@old') . "\n"
-        . 'return $_[0]; ' . "\n"
-    . ' }' . "\n"
-    . $self->_inline_check_lazy($inv) . "\n"
-    . $self->_inline_post_body(@_) . "\n"
-    . 'return ' . $self->_inline_auto_deref($self->_inline_get($inv)) . "\n"
-    . ' }');
-}
-
-sub _generate_writer_method_inline {
-    my $self        = $_[0];
-    my $attr        = $self->associated_attribute;
-    my $attr_name   = $attr->name;
-    my $inv         = '$_[0]';
-    my $slot_access = $self->_inline_get($inv, $attr_name);
-    my $value_name  = $self->_value_needs_copy ? '$val' : '$_[1]';
-
-    $self->_eval_code('sub { '
-    . $self->_inline_pre_body(@_)
-    . $self->_inline_copy_value
-    . $self->_inline_check_required
-    . $self->_inline_check_coercion($value_name)
-    . $self->_inline_check_constraint($value_name)
-    . $self->_inline_store($inv, $value_name)
-    . $self->_inline_post_body(@_)
-    . $self->_inline_trigger($inv, $value_name)
-    . 'return $_[0]; ' . "\n"
-    . ' }');
+sub _inline_post_body {
+return 'return $_[0] if (scalar(@_) >= 2);' . "\n";
 }
 
 1;
